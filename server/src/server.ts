e1a65319ac6aa34f28c0f7c1ae30ef9b67efc7a8
Emerson Moreira <eemr3@yahoo.com.br>
2020-08-10 11:41:11 -0300

    Ajustando o projeto

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,6 @@ app.use(express.json()) //Configurando o express para entender o formato json
 app.use(routes)
 
 //Ligando o servidor
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Server rodando na porta 3333')
 })
